113b8d99d4e07d1b5d4bd34db888ccb62368995b
Gabriel Eaton <50244998+ge64@users.noreply.github.com>
2024-02-23 16:43:38 -0500

    added example data

---
+++ src/exampleEvent.js
@@ -0,0 +1,38 @@
+export const demoEvents = [
+    {
+        title: 'Big Meeting',
+        allDay: true,
+        start   : new Date(2024, 1, 1), // Note: JavaScript months are 0-based
+        end: new Date(2024, 1, 1),
+    },
+    {
+        title: 'Vacation',
+        start: new Date(2024, 1, 7),
+        end: new Date(2024, 1, 10),
+    },
+    {
+        title: 'Team Outing',
+        allDay: true,
+        start: new Date(2024, 1, 5),
+        end: new Date(2024, 1, 6),
+    },
+    {
+        title: 'Project Deadline',
+        allDay: false,
+        start: new Date(2024, 1, 15, 9, 0),
+        end: new Date(2024, 1, 15, 17, 0),
+    },
+    {
+        title: 'Client Meeting',
+        allDay: true,
+        start: new Date(2024, 1, 20),
+        end: new Date(2024, 1, 21),
+    },
+    {
+        title: 'Office Maintenance',
+        allDay: true,
+        start: new Date(2024, 1, 25),
+        end: new Date(2024, 1, 28),
+    },
+];
+
