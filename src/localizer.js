3a3e2f5e788fc7cb3ad5c4843cb2ebf5a6cd599d
Gabriel E <50244998+ge64@users.noreply.github.com>
2024-02-28 00:14:39 -0500

    Delete src directory

--- src/localizer.js
+++
@@ -1,5 +0,0 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-
-// Set up the localizer by providing moment (or globalize) to your calendar
-const localizer = momentLocalizer(moment);
